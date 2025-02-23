330d45078354229c28fc80671ee61c3995d0a006
Muhammad Zikri <83739556+xsm-syn@users.noreply.github.com>
2025-02-23 13:41:38 +0700

    Update api.js

--- api.js
+++ api.js
@@ -101,7 +101,7 @@ app.get('/check', async (req, res) => {
             longitude: "",
             httpProtocol: "",
             tlsVersion: "",
-            source: "@bexnxx"
+            source: "bexnxx"
             //error: error.message
         });
     }
